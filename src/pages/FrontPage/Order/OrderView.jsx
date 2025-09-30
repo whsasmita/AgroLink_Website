be117faba98b284f84e871153c86f9ed03817732
Diar Sudiar <163659290+DiarCode11@users.noreply.github.com>
2025-09-30 21:55:21 +0800

    style: addd tab in onder list

--- src/pages/FrontPage/Order/OrderView.jsx
+++ src/pages/FrontPage/Order/OrderView.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import OrderList from "../../../components/fragments/list/OrderList";
 
 const API = import.meta.env.VITE_SERVER_DOMAIN;
@@ -13,6 +14,7 @@ function PriceIDFormat(price){
 }
 
 export default function OrderView(){
+    const [status, setStatus] = useState("dikemas");
     return (
         <>
             <div className="min-h-screen bg-gray-50 pt-10">
@@ -20,10 +22,21 @@ export default function OrderView(){
                 <div className="flex justify-center">
                     <div className="w-[600px] min-h-[400px]">
                         <div className="bg-white w-full h-full rounded-md shadow-md p-5">
-                            <div className="w-full border-b flex justify-between">
+                            <div className="w-full border-b justify-between">
                                 <h1 className="text-lg font-semibold pb-5">Pesanan Saya</h1>
+                                <div className="grid grid-cols-3">
+                                    <button onClick={() => setStatus("dikemas")} className={`w-full ${status == "dikemas" && "border-b-2 border-green-600 font-semibold text-green-600"}`}>
+                                        Dikemas
+                                    </button>
+                                    <button onClick={() => setStatus("dikirim")} className={`w-full ${status == "dikirim" && "border-b-2 border-green-600 font-semibold text-green-600"}`}>
+                                        Dikirim
+                                    </button>
+                                    <button onClick={() => setStatus("selesai")} className={`w-full ${status == "selesai" && "border-b-2 border-green-600 font-semibold text-green-600"}`}>
+                                        Selesai
+                                    </button>
+                                </div>
                             </div>
-                            <OrderList />
+                            <OrderList status={status} />
                         </div>
                     </div>
                 </div>
