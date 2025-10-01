7710a0219d730b95fbe926e0e29e1fe72e6d84e0
darmawguna <komangagus642@gmail.com>
2025-10-01 12:28:57 +0800

    tes

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = "https://api.goagrolink.com/api/v1";
-export const BASE_URL = "http://localhost:8080/api/v1";
-export const WS_URL = "ws://localhost:8080/api/v1/ws";
+export const BASE_URL = "https://api.goagolink.com/api/v1";
+export const WS_URL = "ws://api.goagolink.com/api/v1/ws";
