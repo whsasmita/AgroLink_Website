60c34c955582e6492c1771f642f8d5f136757109
wahyu harta sasmita <wahyusasmitaharta561@gmail.com>
2025-10-21 17:10:56 +0800

    udae

--- src/services/productServices.js
+++ src/services/productServices.js
@@ -1,29 +1,30 @@
 import { BASE_URL } from "../constants/api";
 
 export async function getProducts() {
-    const token = localStorage.getItem("token");
-    const response = await fetch(`${BASE_URL}/public/products/`, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    });
-    if (!response.ok) {
-        throw new Error("Failed to fetch products");
-    }
-    return response.json();
+  const token = localStorage.getItem("token");
+  const response = await fetch(`${BASE_URL}/public/products/`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (!response.ok) {
+    throw new Error("Failed to fetch products");
+  }
+  return response.json();
 }
 
 export async function getProductsById(productId) {
-    const token = localStorage.getItem("token");
-    const response = await fetch(`${BASE_URL}/public/products/${productId}`, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    });
-    if (!response.ok) {
-        throw new Error("Failed to fetch product details");
-    }
-    return response.json();
+  const token = localStorage.getItem("token");
+  const response = await fetch(`${BASE_URL}/public/products/${productId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (!response.ok) {
+    throw new Error("Failed to fetch product details");
+  }
+  return response.json();
+}
 
 // GET DETAIL PRODUCT DATA
 export async function getProductsById(id) {
