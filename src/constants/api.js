6c45d3e362f5d52efd7c6c2f216c3889716b4995
darmawguna <komangagus642@gmail.com>
2025-11-19 10:14:12 +0800

    update BASE_URL to use production API endpoint

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,5 +1,5 @@
+export const BASE_URL = "https://api.goagrolink.com/api/v1";
 // export const BASE_URL = "https://api.goagrolink.com/api/v1";
-// export const BASE_URL = "https://api.goagrolink.com/api/v1";
-export const BASE_URL = "http://localhost:8080/api/v1";
+// export const BASE_URL = "http://localhost:8080/api/v1";
 export const WS_URL = "ws://api.goagrolink.com/api/v1/ws";
 // update terbaru
